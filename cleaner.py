097fccea8fbf7c06e00482797156b6325336da74
FADHLI NURIL <146806462+fadhlinuril67@users.noreply.github.com>
2024-02-02 20:42:54 +0100

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 def clean_html_from_files(folder_path):
     for filename in os.listdir(folder_path):
@@ -8,11 +9,18 @@ def clean_html_from_files(folder_path):
             with open(file_path, 'r', encoding='utf-8') as file:
                 lines = file.readlines()
             
-            # Check if 'OK' is in the first line and remove it if true
-            if lines and 'OK' in lines[0]:
-                lines = lines[1:]
+            # Preprocess lines to remove those containing ANSI escape sequences
+            cleaned_lines = []
+            ansi_escape = re.compile(r'\x1B[@-_][0-?]*[ -/]*[@-~]')
+            for line in lines:
+                if not ansi_escape.search(line):
+                    cleaned_lines.append(line)
+                elif 'OK' in line:
+                    # This checks for 'OK' specifically in lines with ANSI codes
+                    # If you still want to remove lines with 'OK', regardless of ANSI codes
+                    continue
 
-            file_content = ''.join(lines)
+            file_content = ''.join(cleaned_lines)
             start_index = file_content.find('<')
             end_index = file_content.rfind('>')
             if start_index != -1 and end_index != -1 and end_index > start_index:
